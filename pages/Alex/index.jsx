aeb3892c9fc916af52908d1a0f920a1ade7fc754
Alex <afriederich981@gmail.com>
2022-11-16 16:01:20 -0700

    added: the_meaning_of_life.jpg

--- pages/Alex/index.jsx
+++ pages/Alex/index.jsx
@@ -2,6 +2,9 @@ export default function Alex() {
     return (
       <div>
         <h1>The Void welcomes You.</h1>
+        <p>Welcome to my page! Here you will find the wonder of the universe.</p>
+        <h3>BEHOLD:</h3>
+        <p>alt text: the meaning of life</p>
       </div>
   
     )
