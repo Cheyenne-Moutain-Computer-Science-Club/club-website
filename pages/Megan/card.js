baad7d904d434b3d7b8b64af8253d716a28aa41e
Megan Newkirk-Student <118398987+mrnewk19@users.noreply.github.com>
2022-11-30 21:40:48 +0000

    added elements to my page (fun card element :))

---
+++ pages/Megan/card.js
@@ -0,0 +1,11 @@
+export default function Card() {
+  return(
+    <div>
+    <h1>Megan</h1>
+      <p> Hi this is Megan, she's kinda cool</p>
+    
+
+    </div>
+  )
+  
+}
--- pages/Megan/index.js
+++ pages/Megan/index.js
@@ -1,8 +1,12 @@
+import Card from "./card";
+
 export default function Megan() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>G'day Mateys</h1>
+      <Card />
     </div>
+    
 
   )
 }
