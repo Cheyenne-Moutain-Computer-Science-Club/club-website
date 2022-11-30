4f7bb9df14c9a75b14ec4948b18e26040e955277
AVOnyx <89320518+AVOnyx@users.noreply.github.com>
2022-11-30 21:37:57 +0000

    Added card.jsx and linked it into index.jsx

---
+++ pages/Alex/card.jsx
@@ -0,0 +1,7 @@
+export default function Card() {
+    return (
+      <div>
+        <p>My favorite flavor of icecream is peanut butter cup.</p>
+      </div>
+    )
+  }
--- pages/Alex/index.jsx
+++ pages/Alex/index.jsx
@@ -1,10 +1,13 @@
+import React from 'react';
+import Card from './card';
+import Link from 'next/link';
+
 export default function Alex() {
     return (
       <div>
+        <Link href="/..">Back</Link>
         <h1>The Void welcomes You.</h1>
-        <p>Welcome to my page! Here you will find the wonder of the universe.</p>
-        <h3>BEHOLD:</h3>
-        <p>alt text: the meaning of life</p>
+        <Card />
       </div>
   
     )
