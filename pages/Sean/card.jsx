b31a636ac36b7446e4faf43cd9909129e6f212d7
Sean Wheeler-Student <113160285+ChromiumCranium@users.noreply.github.com>
2022-11-30 21:38:52 +0000

    Sean folder and Card and Thingy comps

---
+++ pages/Sean/card.jsx
@@ -0,0 +1,16 @@
+export default function Card(){
+  return(
+    <div>
+    <h1>Sean Wheeler</h1>
+      <p>Why are there so many errors????</p>
+    </div>
+  )
+}
+
+export default function Thing1(){
+  return(
+    <div>
+     <h2>Ok thas odd</h2>
+    </div>
+  )
+}
---
+++ pages/Sean/index.jsx
@@ -0,0 +1,13 @@
+import Card from "./card";
+import Thing1 from "./card";
+
+export default function Sean(){
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Card />
+      <Thing1 />
+      <Card />
+    </div>
+  )
+}
