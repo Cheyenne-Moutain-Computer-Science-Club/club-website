99c8c8f207bc170cd4fd091a07f4e284b5603c04
Mike <60718603+Mike968@users.noreply.github.com>
2022-11-30 21:13:24 +0000

    AHH

---
+++ pages/Michael/index.jsx
@@ -0,0 +1,9 @@
+export default function Michael() {
+  return (
+    <div className="min-h-screen flex justify-center bg-gradient-to-r from-cyan-700 via-indigo-400 to-blue-500">
+      <div class="w-72 h-72 bg-white bg-opacity-20 backdrop-blur-lg rounded drop-shadow-xl flex justify-center">
+        <h1 className="text-white font-mono text-4xl p-40">Hello World</h1>
+      </div>
+    </div>
+  )
+}
