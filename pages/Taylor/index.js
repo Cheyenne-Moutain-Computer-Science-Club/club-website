6b212fb6077faebb8f36da23893f3c6954c23e66
taylor <94285168+Insequearaux@users.noreply.github.com>
2022-12-07 21:54:37 +0000

    tee hee

--- pages/Taylor/index.js
+++ pages/Taylor/index.js
@@ -1,7 +1,11 @@
 export defualt function Taylor() {
   return (
     <div>
+    <div>
       <h1>Hello World</h1>
+      <Link href="/">Home</Link>
+      <img src="/sillyfunnyguy" alt="luh mao" className="w-9 h-9"/>
+    </div>
     </div>
   )
 }
